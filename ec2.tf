c7b37a8a46dddb1a2a2a29d63166cc85a6b66a3f
nidab024 <nidabot984@google.com>
2025-08-01 00:14:05 +0530

    rebase check from second branch

--- ec2.tf
+++ ec2.tf
@@ -132,3 +132,5 @@ resource "aws_instance" "web_server" {
   ami           = "ami-0c55b159cbfafe1f0" # Ubuntu 20.04 LTS
   instance_type = "t2.medium" # second local
 }
+
+# add line for git rebase check
