22809a6afd09e09d6cfd206dd4c66d339f30d369
nidab024 <nidab984@gmail.com>
2025-07-31 19:45:13 +0530

    ec2 small

--- ec2.tf
+++ ec2.tf
@@ -128,3 +128,7 @@ output "instance_private_dns" {
   value       = aws_instance.linux_instance.private_dns
 }
 */
+resource "aws_instance" "web_server" {
+  ami           = "ami-0c55b159cbfafe1f0" # Ubuntu 20.04 LTS
+  instance_type = "t2.small" # Typo here ("micro" is misspelled)
+}
