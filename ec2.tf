d8961a6dad9db93663e84e8679731726a375fd89
nidab024 <nidabot984@google.com>
2025-07-31 22:01:55 +0530

    nano update on first branch

--- ec2.tf
+++ ec2.tf
@@ -130,5 +130,5 @@ output "instance_private_dns" {
 */
 resource "aws_instance" "web_server" {
   ami           = "ami-0c55b159cbfafe1f0" # Ubuntu 20.04 LTS
-  instance_type = "t2.small" # Typo here ("micro" is misspelled)
+  instance_type = "t2.nano" # Typo here ("micro" is misspelled)
 }
